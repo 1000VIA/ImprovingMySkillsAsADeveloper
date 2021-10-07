fd248fa6f74cf75d7e62311fff69ff5e230490cf
1000VIA <milvampa1@gmail.com>
2021-10-06 22:48:20 -0500

    course finiched

--- React JS/react-router/src/components/Modal.jsx
+++ React JS/react-router/src/components/Modal.jsx
@@ -12,12 +12,12 @@ const Modal = (props) => {
     if(open){
         return ReactDOM.createPortal(
             <div className='ui dimmer show modals visible active' >
-            <div className='ui raised very padded text container segment' >
-                <div className='close'>
-                    <i className='close icon' onClick={closeModal}></i>
+                <div className='ui raised very padded text container segment' >
+                    <div className='close'>
+                        <i className='close icon' onClick={closeModal}></i>
+                    </div>
+                    <h1>I am Modal</h1>
                 </div>
-                <h1>I am Modal</h1>
-            </div>
         </div>,
          document.getElementById('modal')
         )
