bedd37ce3256ae6655ff760d77b8c63ae25e227a
1000VIA <milvampa1@gmail.com>
2021-10-14 19:56:43 -0500

    sesion 10

---
+++ Typescript/ts-node/src/index.ts
@@ -0,0 +1,18 @@
+import { Hero } from "./classes/Hero";
+// import * as HeroClasses from "./classes/Hero";
+// import  powers  from "./data/powers";
+
+
+
+
+// const iroman = new SuperHeroe('Iroman', 12345, 32)
+const iroman = new Hero('Iroman', 10, 32)
+
+console.log( iroman );
+console.log( iroman.power );
+
+
+
+
+
+
