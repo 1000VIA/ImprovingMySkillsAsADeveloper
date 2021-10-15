d6cdd13521da188a25ff4beedaf21ff0df30f8c4
1000VIA <milvampa1@gmail.com>
2021-10-14 19:00:40 -0500

    Sesion 9

--- Typescript/bases/main.js
+++ Typescript/bases/main.js
@@ -1,86 +1,14 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Súper velocidad', 'Viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3 ? true : false);
     };
-    let superman = {
-        name: 'Clark ken',
-        age: 60,
-        powers: ['Súper velocidad'],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
     };
-})();
-(() => {
-    class Mutant {
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Milvia',
-        age: 33,
-        address: {
-            id: 123,
-            zip: 'KFL SUD',
-            city: 'Medellin'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Dayana',
-        age: 18,
-        address: {
-            id: 123,
-            zip: 'KFL SUD',
-            city: 'Medellin'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFuncion;
-    addNumbersFuncion = (a, b) => a * b;
-    console.log(addNumbersFuncion(30, 15));
-})();
-(() => {
-    const conducirBatimovil = (auto) => {
-        auto.encender = true;
-        auto.velocidadMaxima = 100;
-        auto.acelear();
-    };
-    const batimovil = {
-        encender: false,
-        velocidadMaxima: 0,
-        acelear() {
-            console.log("...... gogogo!!!");
-        }
-    };
-    const guason = {
-        reir: true,
-        comer: true,
-        llorar: false
-    };
-    const reir = (guason) => {
-        if (guason.reir) {
-            console.log("JAJAJAJA");
-        }
-    };
-    const ciudadGotica = (ciudadanos) => {
-        return ciudadanos.length;
-    };
-    class Persona {
-        imprimirBio() {
-        }
-    }
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Milvia'));
 //# sourceMappingURL=main.js.map
